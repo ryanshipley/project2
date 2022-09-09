1eedf194f198142022e0ebeffb2280a475cb1576
Ryan Shipley <ryanshipley23@gmail.com>
2022-09-08 23:30:18 -0400

    adds create route

--- server.js
+++ server.js
@@ -1,3 +1,27 @@
+// Dependencies
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Team = require("./models/team.js");
+require("dotenv").config();
+const methodOverride = require("method-override");
+const db = mongoose.connection;
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+db.on("error", (err)=> console.log(err.message + " is mongo not running?"));
+db.on("connected", () => console.log("mongo connected"));
+db.on("disconnected", () => console.log("mongo disconnected"));
+
+// Middleware
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+
+// CREATE
+app.post("/teams", (req, res) =>{
+    Team.create(req.body, (error, createdTeam)=>{
+        res.redirect("/teams");
+    });
+});
