6c8a2171ce59aa7185d052f2be1e20411505984e
Ryan Shipley <ryanshipley23@gmail.com>
2022-09-14 08:40:53 -0400

    completes player model

--- models/player.js
+++ models/player.js
@@ -1,1 +1,14 @@
 const mongoose = require("mongoose");
+
+const playerSchema = new mongoose.Schema({
+    name: String,
+    pos: String,
+    country: String,
+    club: String,
+    caps: Number,
+    goals: Number,
+    age: Number,
+});
+
+const Player = mongoose.model("Player", playerSchema);
+module.exports = Player;
